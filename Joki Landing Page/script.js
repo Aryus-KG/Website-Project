727801797bbf84f5d3bc3183ede4f2a0f866bc1c
Phntme <aryushere@gmail.com>
2025-09-30 20:09:58 +0700

    update 2 section
    Hari ini mayan produktif yah, ngelarin section service dan mulali nyicil testimoni section. di section testimoni masih ada bug yaitu ketika dipencet next arrow, posisi cardnya gamau tengah (gw masih belom tau penyebabnya :V). stress bet gweh di bagian testimoni awakoakw

--- Joki Landing Page/script.js
+++ Joki Landing Page/script.js
@@ -1,3 +1,4 @@
+// navigation
 const menuBtn = document.querySelector("#menu-btn");
 const navbarMenu = document.querySelector("#navbar__menu");
 
@@ -15,3 +16,27 @@ function menuClick() {
 }
 
 menuBtn.addEventListener("click", menuClick);
+
+// testimoni arrow
+const arrowPrev = document.querySelector("#testi-prev");
+const arrowNext = document.querySelector("#testi-next");
+const testimoniCard = document.querySelectorAll(".testimoni__list--item");
+const testimoniContainer = document.querySelector(".testimoni__list");
+
+let testiIndex = 0;
+
+function testiGeser() {
+  testimoniContainer.style.transform = `translateX(-${testiIndex * 100}%)`;
+}
+
+arrowNext.onclick = () => {
+  testiIndex++;
+  if (testiIndex >= testimoniCard.length) testiIndex = 0;
+  testiGeser();
+};
+
+arrowPrev.onclick = () => {
+  testiIndex--;
+  if (testiIndex < 0) testiIndex = testimoniCard.length - 1;
+  testiGeser();
+};
